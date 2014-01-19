81b2912adf8cc7554139c768a614b09438462d20
pR0Ps <pR0Ps.CM@gmail.com>
2014-01-18 19:09:59 -0500

    Add a testing level file

---
+++ levels/1.js
@@ -0,0 +1,36 @@
+[
+    {
+        "initial":{
+            "cursor": [0, 0],
+            "text": [
+                "text",
+                "so wow"
+            ]
+        },
+        "final":{
+            "cursor": [1, 5],
+            "text": [
+                "text",
+                "so wow"
+            ]
+        },
+        "operations": ["hjkl"]
+    },
+    {
+        "initial":{
+            "cursor": [0, 0],
+            "text": [
+                "text",
+                "so wow"
+            ]
+        },
+        "final":{
+            "cursor": null,
+            "text": [
+                "such text",
+                "so wow"
+            ]
+        },
+        "operations": ["i"]
+    }
+]
